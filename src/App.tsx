cd15678e456e0e0a6dae4869ecf7bcc463208094
threis <41017137+threis@users.noreply.github.com>
2021-06-01 08:47:08 -0300

    add files to project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { GlobalStyle } from "./styles/global";
+
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./components/hooks/useTransactions";
+
+Modal.setAppElement("#root");
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
